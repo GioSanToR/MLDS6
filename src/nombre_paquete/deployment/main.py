cfd4616f496533efcd5b22986a0d7d292fb33363
GioSanToR <126033865+GioSanToR@users.noreply.github.com>
2023-06-27 19:51:31 -0500

    Update main.py

--- src/nombre_paquete/deployment/main.py
+++ src/nombre_paquete/deployment/main.py
@@ -1,1 +1,29 @@
+import tensorflow as tf
+import numpy as np
+from music21 import *
 
+from fastapi import FastAPI
+from fastapi.response import FileResponse
+from pydantic import BaseModel
+
+file_path = '/GioSanToR/MLDS6/tree/main/src/nombre_paquete/deployment'
+
+model = tf.keras.models.load_model('LSTM.h5')
+
+app = FastAPI()
+
+class UserInput(BaseModel):
+    user_input: int
+
+@app.get('/')
+async def index():
+    return {"Message": "Alive"}
+
+@app.post('/midi/')
+async def predict(UserInput: UserInput):
+    if user_input > 0:
+      prediction = midi_gen([UserInput.user_input])
+    else:
+      return ("número inválido de notas")
+
+    return FileResponse(path = file_path, filename = prediction)
